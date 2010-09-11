051ad748321d21e180c39ba191b4188e8efbe967
David Tong <david.tong@digiaid.com.au>
2010-09-11 14:30:04 +1000

    * Gift scaffold

---
+++ db/migrate/20100911042740_create_gifts.rb
@@ -0,0 +1,15 @@
+class CreateGifts < ActiveRecord::Migration
+  def self.up
+    create_table :gifts do |t|
+      t.string :description
+      t.boolean :is_admin
+      t.date :purchased_on
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :gifts
+  end
+end
