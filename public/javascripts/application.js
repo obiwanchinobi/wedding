7b707bcb09ae490d502aae7fd3177bc730ef8e36
David Tong <david.tong@digiaid.com.au>
2010-09-14 00:13:43 +1000

    * Display claim date

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,18 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 jQuery(document).ready(function($) {
-  $('a[rel*=facebox]').facebox()
+  $('a[rel*=facebox]').facebox();
+  
+  $("tr").hover(
+    function () {
+      $(this).find("span.claimed").removeClass("claimed").addClass("claimed-hover");
+      $(this).find("a.claimed-link").removeClass("claimed-link").addClass("claimed-link-hover");
+      $(this).find("span.claimed-timestamp").removeClass("ui-helper-hidden");
+    },
+    function () {
+      $(this).find("span.claimed-hover").removeClass("claimed-hover").addClass("claimed");
+      $(this).find("a.claimed-link-hover").removeClass("claimed-link-hover").addClass("claimed-link");
+      $(this).find("span.claimed-timestamp").addClass("ui-helper-hidden");
+    }
+  );
 })
